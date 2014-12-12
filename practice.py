7d5e00aa82e882313c37dbee763a177e9ca5ea26
ampammam34 <ampammam34@gmail.com>
2014-12-12 14:54:42 +0900

    new

--- practice.py
+++ practice.py
@@ -14,28 +14,38 @@ import math
 #        s = math.sin(th[i])
 #        c = math.cos(th[i])
 
+
 def rotationXandOffset(x, y, z, th):
+    th = [0, 0, 0, 0, 0, 0, 0]
+    for i in range(7):
+        s = math.sin(th[i])
+        c = math.cos(th[i])
     P = np.array([[1,0,0,x],[0,c,s,y],[0,-s,c,z],[0,0,0,1]])
     return P
 
 def rotationYandOffset(x, y, z, th):
+    th = [0, 0, 0, 0, 0, 0, 0]
+    for i in range(7):
+        s = math.sin(th[i])
+        c = math.cos(th[i])
     P = np.array([[c,0,s,x],[0,1,0,y],[-s,0,c,z],[0,0,0,1]])
     return P
 
 def rotationZandOffset(x, y, z, th):
+    th = [0, 0, 0, 0, 0, 0, 0]
+    for i in range(7):
+        s = math.sin(th[i])
+        c = math.cos(th[i])
     P = np.array([[c,s,0,x],[-s,c,0,y],[0,0,1,z],[0,0,0,1]])
     return P
 
 if __name__ == '__main__':
-
+    
     l1 = 10
     l2 = 12
     l3 = 15
     th = [0, 0, 0, 0, 0, 0, 0]
     T = [0]*7
-    for i in range(7):
-        s = math.sin(th[i])
-        c = math.cos(th[i])
 
     T1 = rotationYandOffset(0, 0, 0, th[0])
     T2 = rotationXandOffset(0, 0, 0, th[1])
